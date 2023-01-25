419a7fe732136ddd0838ae0f0ef1a8870a6ec564
RaltsHunter <117216708+RaltsHunter@users.noreply.github.com>
2023-01-25 14:08:12 -0600

    Added playable version of exercise one

---
+++ exercises/exercise01/Assets/Credits.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     //Flying saucer by Poly by Google[CC - BY] via Poly Pizza
+     //Pizza Steve by Stephen Myer[CC - BY] via Poly Pizza
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
